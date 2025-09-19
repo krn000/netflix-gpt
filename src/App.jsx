63efdc9429cbf3a08795c6e22218584bcb9f9cdb
Karan Mishra <karan.mishra@fretron.com>
2025-09-19 12:20:06 +0530

    SignIN and SignUo Form

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
+import Body from "./components/Body";
+
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline text-green-600">Hello world!</h1>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+     <Body />
     </>
   );
 }
---
+++ src/components/Login.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react'
+import Header from './Header'
+
+const Login = () => {
+
+  const [isSignIn, setIsSignIn] = useState(true)
+  const background = "https://assets.nflxext.com/ffe/siteui/vlv3/c95abc7a-8124-4630-bb7a-3b160bdc6de3/web/IN-en-20250915-TRIFECTA-perspective_d3d87aa7-58ed-4c6b-98dc-231ed05ba675_medium.jpg"
+
+  const toggleSignUp = () => {
+    setIsSignIn(!isSignIn)
+  }
+  return (
+    <>
+      <Header />
+      <img src={background} alt="background" className='w-full h-screen' />
+      <div className='absolute top-0 left-0 w-full h-full bg-black/50 flex justify-center items-center bg-opacity-80'>
+        <form className='bg-black/50 p-10 rounded-lg w-4/12'> 
+          
+          <h1 className='text-4xl font-bold text-white'>{isSignIn ? 'Sign In' : 'Sign Up'}</h1>
+          <input type="email" placeholder='Email or phone number' className='p-2 my-4 w-full bg-gray-700 rounded-md text-white' />
+          {!isSignIn && <input type="name" placeholder='Name' className='p-2 my-4 w-full bg-gray-700 rounded-md text-white' />}
+
+          <input type="password" placeholder='Password' className='p-2 my-4 w-full bg-gray-700 rounded-md text-white' />
+          <button className='p-2 my-4 w-full bg-red-700 text-white rounded-md'>{isSignIn ? 'Sign In' : 'Sign Up'}</button>
+          <p className='text-sm text-white'>{isSignIn ? 'New to Netflix?' : 'Already have an account?'} <a href="#" className='pl-3 font-bold' onClick={toggleSignUp}>{isSignIn ? 'Sign up now.' : 'Sign in now.'}</a></p>
+        </form>
+      </div>
+    </>
+  )
+}
+
+export default Login
